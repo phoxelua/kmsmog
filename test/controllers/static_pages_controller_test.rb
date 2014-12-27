6187ed0121e87b329722d75294981646a2ce2722
phoxelua <howardanguyen@gmail.com>
2014-12-26 17:05:16 -0800

    Added static pages home help

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | KMSC"    
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | KMSC"    
+  end
+
+end
