fc444561db0f38ca5bfa6f252abc3d7c4bb50777
phoxelua <howardanguyen@gmail.com>
2014-12-28 18:33:21 -0800

    Finished updating and editing. Starting delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :dashboard]
+  before_action :logged_in_user, only: [:edit, :update, :dashboard, :index]
   before_action :correct_user,   only: [:edit, :update, :dashboard]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
   	@user = User.find(params[:id])
